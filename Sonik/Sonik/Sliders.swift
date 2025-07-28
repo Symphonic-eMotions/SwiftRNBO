81f00dcac30aff040260c91ee78cda7fca8d452e
Symphonic eMotions <info@symphonic-emotions.nl>
2025-07-29 00:40:50 +0200

    Icon en mapstructuur

--- Sonik/Sonik/Sliders.swift
+++ Sonik/Sonik/Sliders.swift
@@ -5,41 +5,39 @@
 //  Created by Эльдар Садыков on 13.02.2023.
 //
 
-#if !os(tvOS)
-    import SwiftUI
+import SwiftUI
 
-    struct Sliders: View {
-        @EnvironmentObject var rnbo: RNBOAudioUnitHostModel
+struct Sliders: View {
+    @EnvironmentObject var rnbo: RNBOAudioUnitHostModel
 
-        var body: some View {
-            ScrollView {
-                VStack {
-                    ForEach($rnbo.parameters) { $parameter in
-                        SliderView(parameter: $parameter)
-                    }
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach($rnbo.parameters) { $parameter in
+                    SliderView(parameter: $parameter)
                 }
-                .padding()
-                .background()
-                .padding(.bottom)
             }
-            .frame(minHeight: 100)
+            .padding()
+            .background()
             .padding(.bottom)
         }
+        .frame(minHeight: 100)
+        .padding(.bottom)
     }
+}
 
-    struct SliderValueLabel: View {
-        let value: Double
-        var body: some View {
-            Text(String(format: "%.2f", value))
-                .frame(minWidth: 50)
-        }
+struct SliderValueLabel: View {
+    let value: Double
+    var body: some View {
+        Text(String(format: "%.2f", value))
+            .frame(minWidth: 50)
     }
+}
 
-    struct SliderNameLabel: View {
-        let name: String
-        var body: some View {
-            Text(name)
-                .frame(minWidth: 100)
-        }
+struct SliderNameLabel: View {
+    let name: String
+    var body: some View {
+        Text(name)
+            .frame(minWidth: 100)
     }
-#endif
+}
